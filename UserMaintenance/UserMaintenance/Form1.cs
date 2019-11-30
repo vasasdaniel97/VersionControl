3e7796e00c73917d25b08080bc9e490a573331f4
Dániel Vasas <daniel.vasas@stud.uni-corvinus.hu>
2019-11-30 18:09:32 +0100

    hal

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,6 +15,9 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.String1; // label1
+            label2.Text = Resource1.String2; // label2
+            button1.Text = Resource1.String3; // button1
         }
     }
 }
